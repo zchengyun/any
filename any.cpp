a570df0fddf8074b4b2e993e4cbe1cec3cb96c2f
zchengyun <1690132420@qq.com>
2021-08-31 23:35:05 +0800

    添加项目文件。

---
+++ any.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <any>
+int main()
+{
+    std::any  a = 1;
+    if (a.has_value()) {
+        std::cout << a.type().name()<<std::endl;
+    }
+
+    a = "hello";
+    std::cout << a.type().name() << std::endl;
+
+    a = std::string{"word"};
+    std::cout << a.type().name() << std::endl;
+
+    auto s = std::make_any<std::string>("hello world");
+    if (s.type() == typeid(std::string)) {
+        auto a = std::any_cast<std::string>(s);
+        std::cout << a << std::endl;
+    }
+
+    //要访问包含的值，必须使用std::any_cast<>将其转换为其类型。
+    //auto b = s;
+    //std::cout << b << std::endl;//错误
+
+}
+
